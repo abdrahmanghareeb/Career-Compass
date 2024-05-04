a7c2b9bf7f1bd2d584fa16ef7207007d3898d648
Abdrahman Ghareeb <abdrahmanghareeb@gmail.com>
2024-05-04 22:34:48 +0300

    profile screen

---
+++ lib/Layouts/ProfileScreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("data"),
+            ],
+          ),
+        ),
+    );
+  }
+
+
+}
---
+++ lib/Shared/Constants/color.dart
@@ -0,0 +1,2 @@
+String main_color = "#50C2C9";
+String appGrey_color = "#0A0A0A";
